c21232568720255b3bfb4948e37b5d70c2611ee8
mengwei <mengwei008@gmail.com>
2021-03-20 19:38:06 +0800

    以Gulp.js 4.x风格重新编写gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,78 @@
-const gulp = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
 const rmfr = require('rmfr');
 const fs = require('fs');
 const connect = require('gulp-connect');
 const puppeteer = require('puppeteer');
-const { series } = require('gulp');
 
-gulp.task('resume-sass', async () => {
-  gulp
-    .src('src/scss/resume.scss')
+function src2dist(dir) {
+  return src(`./src/${dir}/*.*`).pipe(dest(`./dist/${dir}/`));
+}
+
+function cssTranspile() {
+  return src('src/scss/resume.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css/'))
+    .pipe(dest('dist/css/'))
     .pipe(connect.reload());
+}
+
+watch(['./src/scss/resume.scss', './src/scss/content.scss'], function (cb) {
+  series(cssTranspile)();
+  cb();
 });
 
-gulp.task('sass:watch', async () => {
-  gulp.watch('./src/scss/resume.scss', gulp.series('resume-sass'));
-  gulp.watch('./src/scss/content.scss', gulp.series('resume-sass'));
-});
-
-gulp.task('json2pug', async () => {
+function pugTranspile() {
   const locals = JSON.parse(fs.readFileSync('./resume.json', 'utf-8'))
-  gulp
-    .src('./src/pug/index.pug')
+  return src('./src/pug/index.pug')
     .pipe(
       pug({
         locals
       })
     )
-    .pipe(gulp.dest('./dist/'))
+    .pipe(dest('./dist/'))
     .pipe(connect.reload());
-});
-
-gulp.task('json2pug:watch', async () => {
-  gulp.watch('./resume.json', gulp.series('json2pug'));
-  gulp.watch('./src/pug/*.pug', gulp.series('json2pug'));
-});
-
-function src2dist(dir) {
-  return gulp.src(`./src/${dir}/*.*`).pipe(gulp.dest(`./dist/${dir}/`));
 }
 
-gulp.task('copy', async () => {
-  src2dist('fonts');
-  src2dist('pdf');
+watch(['./resume.json', './src/pug/*.pug'], function(cb) {
+  series(pugTranspile)();
+  cb();
 });
 
-gulp.task('clean', async () => {
+function copy(cb) {
+  src2dist('fonts');
+  src2dist('pdf');
+  cb();
+}
+
+function clean(cb) {
   rmfr('./dist/');
-});
+  cb();
+}
 
 let port = 10086;
 
 // 避免打印时，同时运行开发服务报错
-gulp.task('set-pdf-port', async () => {
+function setPdfPort(cb) {
   port = 10010;
-});
+  cb();
+}
 
-gulp.task('webserver', async () => {
-  connect.server({
-    root: './dist',
-    livereload: true,
-    port
-  });
-});
-
-// gulp.task('default', ['resume-sass', 'json2pug', 'copy'])
-gulp.task('default', gulp.series('resume-sass', 'json2pug', 'copy'));
-
-gulp.task('dev', gulp.series('default', 'json2pug:watch', 'sass:watch', 'webserver'));
-
-gulp.task('pdf', series(gulp.parallel('set-pdf-port', 'default', 'webserver'), async () => {
+async function generatePDF() {
   const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
 
-  // In the case of multiple pages in a single browser, each page can have its own viewport size.
+  /**
+   * In the case of multiple pages in a single browser, each page can have its own viewport size.
+   */
   await page.setViewport({
     width: 1440,
     height: 900
   });
 
-  // networkidle0 - consider navigation to be finished when there are no more than 0 network connections for at least 500 ms.
+  /**
+   * networkidle0 - consider navigation to be finished when there are no more than 0 network connections for at least 500 ms.
+   */
   await page.goto('http://localhost:10010', {waitUntil: 'networkidle0'});
 
   await page.pdf({
@@ -98,8 +89,45 @@ gulp.task('pdf', series(gulp.parallel('set-pdf-port', 'default', 'webserver'), a
   });
 
   console.log('PDF已生成, 目录./src/pdf');
-  browser.close();
+  await browser.close();
 
   connect.serverClose();
   process.exit(0);
-}));
+}
+
+function livereload(cb) {
+  connect.server({
+    root: './dist',
+    livereload: true,
+    port
+  });
+  cb();
+}
+
+exports.default = series(
+  cssTranspile,
+  pugTranspile,
+  copy
+);
+
+exports.dev = series(
+  cssTranspile,
+  pugTranspile,
+  copy,
+  livereload
+);
+
+exports.pdf = series(
+  parallel(
+    setPdfPort,
+    series(
+      cssTranspile,
+      pugTranspile,
+      copy,
+      livereload
+    )
+  ),
+  generatePDF
+);
+
+exports.clean = clean;
